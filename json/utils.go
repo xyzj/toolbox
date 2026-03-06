97ba9b15090310351b4b550ebae19bcc19a4da72
unknown <beunknown@wlst.vip>
2026-03-06 15:25:36 +0800

    update json/utils.go

--- json/utils.go
+++ json/utils.go
@@ -9,16 +9,16 @@ import (
 
 // Bytes 内存地址转换string
 func Bytes(s string) []byte {
-	return unsafe.Slice(unsafe.StringData(s), len(s))
+	return *(*[]byte)(unsafe.Pointer(&s))
 }
 
 // String 内存地址转换[]byte
 func String(b []byte) string {
-	return unsafe.String(unsafe.SliceData(b), len(b))
+	return *(*string)(unsafe.Pointer(&b))
 }
 
 // PB2Json pb2格式转换为json []byte格式
-func PB2Json(pb interface{}) []byte {
+func PB2Json(pb any) []byte {
 	jsonBytes, err := Marshal(pb)
 	if err != nil {
 		return nil
@@ -27,7 +27,7 @@ func PB2Json(pb interface{}) []byte {
 }
 
 // PB2String pb2格式转换为json 字符串格式
-func PB2String(pb interface{}) string {
+func PB2String(pb any) string {
 	b, err := MarshalToString(pb)
 	if err != nil {
 		return ""
@@ -36,7 +36,7 @@ func PB2String(pb interface{}) string {
 }
 
 // JSON2PB json字符串转pb2格式
-func JSON2PB(js string, pb interface{}) error {
+func JSON2PB(js string, pb any) error {
 	err := Unmarshal(Bytes(js), &pb)
 	return err
 }
